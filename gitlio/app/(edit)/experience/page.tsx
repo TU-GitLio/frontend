ad56c822a88d85fb98e6dd8d79fb3ae91de20140
junyoung <no2jfamily@tukorea.ac.kr>
2024-02-07 11:50:42 +0900

    feat: experience 가로, 세로 버전 css 초기적용

--- gitlio/app/(edit)/experience/page.tsx
+++ gitlio/app/(edit)/experience/page.tsx
@@ -7,14 +7,139 @@ export default function ExperiencePage() {
   return (
     <>
       <div className="w-full h-full grid bg-primary grid-cols-5 grid-rows-9">
-        <div className="flex justify-center col-span-1 row-span-9 h-full border border-primary">
+        <div className="flex justify-center col-span-0 row-span-9 h-full border border-primary">
           <SideBar />
         </div>
         <EditHeader />
         <div className="col-span-4 row-span-8 h-full bg-primary border border-[#B1C9FF]">
           <div className="flex flex-col h-full items-center px-5 ">
             <Title />
-            <EditBuilder />
+            <EditBuilder>
+              <div className="flex flex-row justify-around items-center h-full">
+                <div className="flex flex-col justify-center basis-96 h-80 text-center box-border border-4 border-primary">
+                  <ul className="w-full timeline flex flex-rol justify-around flex-nowrap px-2">
+                    <li className="shrink-0">
+                      <div className="timeline-start timeline-box">
+                        <input className="border-2 border-primary w-24 h-5" />
+                      </div>
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-0.214-.882l-3.483 4.79-0.88-0.88a.75.75 0 10-0.06 0.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <hr />
+                    </li>
+                    <li className="shrink-0">
+                      <hr />
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-0.214-.882l-3.483 4.79-0.88-0.88a.75.75 0 10-0.06 0.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <input className="border-2 border-primary w-24 h-5" />
+                      <hr />
+                    </li>
+                    <li className="shrink-0">
+                      <hr />
+                      <input className="border-2 border-primary w-24 h-5" />
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-0.214-.882l-3.483 4.79-0.88-0.88a.75.75 0 10-0.06 0.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <hr />
+                    </li>
+                  </ul>
+                </div>
+                <div className="flex flex-col justify-center basis-96 h-80 text-center border-4 border-primary">
+                  <ul className="h-full timeline timeline-vertical flex flex-col justify-between items-center py-4">
+                    <li className="relative">
+                      <input className="border-2 border-primary w-24 h-5" />
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-5 h-5 absolute right-0"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <hr />
+                    </li>
+                    <li>
+                      <hr />
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-5 h-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <input className="border-2 border-primary w-24 h-5" />
+                      <hr />
+                    </li>
+                    <li className="relative">
+                      <hr />
+                      <input className="border-2 border-primary w-24 h-5" />
+                      <div className="timeline-middle">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="w-5 h-5 absolute right-0"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                      <hr />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </EditBuilder>
           </div>
         </div>
       </div>
