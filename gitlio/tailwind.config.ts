05f60134e232fcab402c9d358da7c27a701cae15
seojinJeon <gij0321@naver.com>
2024-02-07 23:10:21 +0900

    feat: 확정된 tailwind.config.ts

--- gitlio/tailwind.config.ts
+++ gitlio/tailwind.config.ts
@@ -13,14 +13,58 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#DDE5FF",
+      },
     },
     screens: {
-      sm: { min: "265px", max: "768px" },
-      md: { min: "620px", max: "1023px" },
-      lg: { min: "1080px" },
+      sm: { max: "768px" },
+      md: { max: "1023px" },
+      lg: { min: "1023px" },
       xl: { min: "1300px" },
+
+      //전서진: 1273.7px (13.3인치, 33.7cm) -> lg
+      //정준영: 1440px (15인치, 38.1cm)     -> xl
+
+      // sm: { min: "265px", max: "768px" },
+      // md: { min: "620px", max: "1023px" },
+      // lg: { min: "1080px" },
+      // xl: { min: "1300px" },
     },
+    mode: "jit",
   },
   plugins: [],
 };
 export default config;
+
+// // //준영님 코드
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./pages//*.{js,ts,jsx,tsx,mdx}",
+//     "./components//.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//       colors: {
+//         primary: "#DDE5FF",
+//       },
+//     },
+//     screens: {
+//       sm: { min: "265px", max: "768px" },
+//       md: { min: "768px", max: "1023px" },
+//       lg: { min: "1023px" },
+//       xl: { min: "1300px" },
+//     },
+//     mode: "jit",
+//   },
+//   plugins: [],
+// };
+// export default config;
