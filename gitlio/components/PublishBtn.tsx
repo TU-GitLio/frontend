63f08dccf3cbfb6e4aafa617fdb339bd35210c25
junyoung <no2jfamily@tukorea.ac.kr>
2024-02-07 20:54:18 +0900

    refactor: width 수정

--- gitlio/components/PublishBtn.tsx
+++ gitlio/components/PublishBtn.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function PublishBtn() {
   return (
-    <div className="w-64">
+    <div className="w-52">
       <button className="bg-[#6385FD] p-3 rounded-xl w-full h-full">
         <span className="text-white">Publish</span>
       </button>
