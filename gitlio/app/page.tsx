11dab3f1aeab693567834cd4dd9dff6931a1424a
ajeong <jenny2360@tukorea.ac.kr>
2024-02-18 23:36:08 -0800

    design: 시작 페이지 레이아웃 구성

--- gitlio/app/page.tsx
+++ gitlio/app/page.tsx
@@ -1,9 +1,31 @@
 import Logo from "@/components/Logo";
 
-export default function EditPage() {
+export default function StartPage() {
   return (
-    <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
-      <Logo />
-    </nav>
+    <div className="bg-primary flex justify-center items-center h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2 absolute top-0 w-full">
+        <Logo />
+      </nav>
+      <div className="bg-primary flex flex-col justify-center items-center h-screen">
+        <div className="relative flex justify-center items-center">
+          <img
+            src="https://i.ibb.co/7WRLdst/Group-69.png"
+            alt="deco icon"
+            className="absolute w-auto h-auto max-w-xs lg:max-w-lg xl:max-w-xl"
+          />
+          <img
+            src="/gitlio.png"
+            alt="Profile Image"
+            className="relative z-10 w-auto h-auto max-w-xs lg:max-w-lg xl:max-w-xl"
+          />
+        </div>
+        <div className="text-center mt-8 text-xl text-[#8288a1] underline underline-offset-4">
+          LOG IN
+        </div>
+        <div className="text-center mt-4 text-xl text-[#8288a1] underline underline-offset-4">
+          SIGN IN
+        </div>
+      </div>
+    </div>
   );
 }
