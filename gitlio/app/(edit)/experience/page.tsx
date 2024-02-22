91fe612223907932c08a89f9d17d64453e8eb960
junyoung <no2jfamily@tukorea.ac.kr>
2024-02-22 19:38:10 +0900

    feat: works1 페이지 구현 시작

--- gitlio/app/(edit)/experience/page.tsx
+++ gitlio/app/(edit)/experience/page.tsx
@@ -2,17 +2,18 @@ import EditBuilder from "@/components/EditBuilder";
 import EditHeader from "@/components/EditHeader";
 import SideBar from "@/components/SideBar";
 import Title from "@/components/Title";
+import { useState } from "react";
 
 export default function ExperiencePage() {
   return (
     <>
       <div className="w-full h-full grid bg-primary grid-cols-5 grid-rows-9">
-        <div className="flex justify-center col-span-1 row-span-9 h-full border border-primary">
+        <div className="flex justify-center col-span-0 row-span-9 h-full border border-primary">
           <SideBar />
         </div>
         <EditHeader />
         <div className="col-span-4 row-span-8 h-full bg-primary border border-[#B1C9FF]">
-          <div className="flex flex-col h-full items-center px-5 ">
+          <div className="flex flex-col h-full items-center px-5">
             <Title />
             <EditBuilder />
           </div>
