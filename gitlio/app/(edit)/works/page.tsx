02ce53783dda15292b7b22e1ec19303ccaaca1e2
junyoung <no2jfamily@tukorea.ac.kr>
2024-02-23 13:25:49 +0900

    feat: radix-ui 설치 및 works페이지 초기 구현

--- gitlio/app/(edit)/works/page.tsx
+++ gitlio/app/(edit)/works/page.tsx
@@ -14,7 +14,32 @@ export default function WorksPage() {
         <div className="col-span-4 row-span-8 h-full bg-[#DCE6FF] border border-[#B1C9FF]">
           <div className="flex flex-col h-full items-center px-5 ">
             <Title />
-            <EditBuilder />
+            <EditBuilder>
+              <div className="flex flex-col justify-around h-full">
+                <div className="flex flex-row justify-around items-center">
+                  <div className="flex flex-row justify-center items-center">
+                    <div className="w-96 h-72 border-4 border-primary">
+                      <div className="flex flex-row h-full justify-between items-center">
+                        <div className="">1</div>
+                        <div className="">2</div>
+                        <div className="">3</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-row justify-center">
+                    <div className="w-96 h-72 border-4 border-primary"></div>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-around">
+                  <div className="flex flex-row justify-center">
+                    <div className="w-96 h-72 border-4 border-primary"></div>
+                  </div>
+                  <div className="flex flex-row justify-center">
+                    <div className="w-96 h-72 border-4 border-primary"></div>
+                  </div>
+                </div>
+              </div>
+            </EditBuilder>
           </div>
         </div>
       </div>
