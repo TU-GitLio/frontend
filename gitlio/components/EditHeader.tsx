28eb9c5e819f4aaa1752f953acd8e12a5945eefc
junyoung <no2jfamily@tukorea.ac.kr>
2024-03-06 10:29:00 +0900

    refactor: 테두리 색깔 변경

--- gitlio/components/EditHeader.tsx
+++ gitlio/components/EditHeader.tsx
@@ -4,7 +4,7 @@ import PublishBtn from "./PublishBtn";
 
 export default function EditHeader() {
   return (
-    <div className="flex items-center justify-end col-span-4 row-span-1 w-full h-full bg-primary border border-primary px-4">
+    <div className="flex items-center justify-end col-span-4 row-span-1 w-full h-full bg-primary border border-[#B1C9FF] px-4">
       <PublishBtn />
       <div className="flex ml-5 w-16 h-16 justify-center items-center">
         <img
