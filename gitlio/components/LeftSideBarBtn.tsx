91499f7bfdafa28a096af4a36888cd496e12073d
junyoung <no2jfamily@tukorea.ac.kr>
2024-03-16 15:12:22 +0900

    refactor: 왼쪽사이드바 버튼 컴포넌트 작업, daisy-ui 적용

--- gitlio/components/LeftSideBarBtn.tsx
+++ gitlio/components/LeftSideBarBtn.tsx
@@ -12,20 +12,20 @@ export default function LeftSideBarBtn() {
     <div className="flex flex-col w-[75%] h-full justify-center sm:justify-center md:justify-center">
       <Link href={"/introduction"}>
         {titleUrl === "introduction" ? (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%]">
-            <div className="bg-white w-6  rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">1</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
-              <span className="">Introduction</span>
+            <div className="mr-auto ml-2 font-semibold text-lg md:truncate sm:truncate">
+              Introduction
             </div>
           </button>
         ) : (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-blue-500 text-white py-2 px-4 my-[7%]">
-            <div className="bg-white w-6  rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-blue-500 text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">1</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold text-white text-lg md:truncate sm:truncate">
               Introduction
             </div>
           </button>
@@ -34,20 +34,20 @@ export default function LeftSideBarBtn() {
 
       <Link href={"/skills"}>
         {titleUrl === "skills" ? (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%]">
-            <div className="bg-white w-6  rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7  rounded-full text-black shrink-0">
               <span className="font-medium">2</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold text-lg md:truncate sm:truncate">
               Skills
             </div>
           </button>
         ) : (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-blue-500 text-white py-2 px-4 my-[7%]">
-            <div className="bg-white w-6  rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-blue-500 text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7  rounded-full text-black shrink-0">
               <span className="font-medium">2</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-white text-lg sm:truncate">
               Skills
             </div>
           </button>
@@ -56,20 +56,20 @@ export default function LeftSideBarBtn() {
 
       <Link href={"/experience"}>
         {titleUrl === "experience" ? (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%]">
-            <div className="bg-white w-6  rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0 outline-none focus:outline-none">
               <span className="font-medium">3</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-lg sm:truncate">
               Experience
             </div>
           </button>
         ) : (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-blue-500 text-white py-2 px-4 my-[7%]">
-            <div className="bg-white w-6  rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-blue-500 text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">3</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-white text-lg sm:truncate">
               Experience
             </div>
           </button>
@@ -78,20 +78,20 @@ export default function LeftSideBarBtn() {
 
       <Link href={"/works"}>
         {titleUrl === "works" ? (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%]">
-            <div className="bg-white w-6 rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">4</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-lg sm:truncate">
               Works
             </div>
           </button>
         ) : (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-blue-500 text-white py-2 px-4 my-[7%]">
-            <div className="bg-white w-6 rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-blue-500 text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">4</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-white text-lg sm:truncate">
               Works
             </div>
           </button>
@@ -100,20 +100,20 @@ export default function LeftSideBarBtn() {
 
       <Link href={"/outro"}>
         {titleUrl === "outro" ? (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%]">
-            <div className="bg-white w-6 rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-[#B1C9FF] text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">5</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-lg sm:truncate">
               Outro
             </div>
           </button>
         ) : (
-          <button className="flex flex-row flex-grow justify-between w-full rounded-full bg-blue-500 text-white py-2 px-4 my-[7%]">
-            <div className="bg-white w-6 rounded-full text-black shrink-0">
+          <button className="flex flex-row border-0 flex-grow justify-between w-full rounded-full bg-blue-500 text-black py-2 px-4 my-[7%] btn btn-active">
+            <div className="flex flex-row justify-center items-center bg-white w-7 h-7 rounded-full text-black shrink-0">
               <span className="font-medium">5</span>
             </div>
-            <div className="mr-auto ml-2 font-medium md:truncate sm:truncate">
+            <div className="mr-auto ml-2 font-semibold md:truncate text-white text-lg sm:truncate">
               Outro
             </div>
           </button>
