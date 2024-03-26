8da2f7587fcf780fdf96d9e97024999d1014553a
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-26 22:37:21 +0900

    design: Editor Top menu layout

--- gitlio/app/editor/[portfolioDomain]/page.tsx
+++ gitlio/app/editor/[portfolioDomain]/page.tsx
@@ -1,3 +1,5 @@
 export default function EditPage() {
-  return <div>여기서 포트폴리오 편집헤여</div>;
+  return (
+      <div>여기서 포트폴리오 편집헤여</div>
+  );
 }
--- gitlio/tailwind.config.ts
+++ gitlio/tailwind.config.ts
@@ -13,26 +13,6 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        primary: "#FFF",
-        selected: "#e5e7eb",
-        sideDark: "#1E293B",
-        black: "#000000 !important",
-      },
-    },
-    screens: {
-      sm: { max: "768px" },
-      md: { max: "1023px" },
-      lg: { min: "1023px" },
-      xl: { min: "1300px" },
-
-      //전서진: 1273.7px (13.3인치, 33.7cm) -> lg
-      //정준영: 1440px (15인치, 38.1cm)     -> xl
-
-      // sm: { min: "265px", max: "768px" },
-      // md: { min: "620px", max: "1023px" },
-      // lg: { min: "1080px" },
-      // xl: { min: "1300px" },
     },
     mode: "jit",
     darkMode: "class",
