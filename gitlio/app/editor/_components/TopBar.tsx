1a5452375c05617b4b836396d160a9b4176e7bc0
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-27 18:56:36 +0900

    design: Editor Top menu layout - 3

---
+++ gitlio/app/editor/_components/TopBar.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import {FaGlobeAsia} from "react-icons/fa";
+import {LuAlignJustify, LuBarChartBig} from "react-icons/lu";
+
+export default function TopBar(){
+    return (
+        <div className="navbar bg-neutral-800">
+            <div className="flex-1">
+
+                <a className="btn btn-ghost text-xl text-white hover:bg-base-300/20">
+                    <Image src={"/logo_white_lg.svg"} alt={"whiteLogo"} width={30} height={30}/>
+                    GITLIO
+                </a>
+            </div>
+            <div className="flex-none gap-6">
+                <a className="btn btn-ghost text-lg text-white hover:bg-base-300/20">
+                    <FaGlobeAsia className="text-white size-6"/>
+                    공유
+                </a>
+                <button className="btn">
+                    <div className="mx-2 text-lg text">저장</div>
+                </button>
+                <button className="btn btn-square btn-ghost">
+                    <LuBarChartBig className="text-white size-6"/>
+                </button>
+                <button className="btn btn-square btn-ghost">
+                    <LuAlignJustify className="text-white size-6"/>
+                </button>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component"
+                                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
+                        </div>
+                    </div>
+                    <ul tabIndex={0}
+                        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
--- gitlio/app/editor/layout.tsx
+++ gitlio/app/editor/layout.tsx
@@ -1,58 +1,28 @@
-import { LuAlignJustify, LuBarChartBig } from "react-icons/lu";
-import { FaGlobeAsia } from "react-icons/fa";
-import Image from "next/image";
+import cx from "classnames"
+import TopBar from "@/app/editor/_components/TopBar";
 
 type Props = {
     children: React.ReactNode;
 }
 export default function EditorLayout({ children } : Props) {
+    const layoutSelected = true
     return (
-        <div>
-            <div className="navbar bg-neutral-800">
-                <div className="flex-1">
-
-                    <a className="btn btn-ghost text-xl text-white hover:bg-base-300/20">
-                        <Image src={"/logo_white_lg.svg"} alt={"whiteLogo"} width={30} height={30}/>
-                        GITLIO
-                    </a>
+        <div className="flex-row">
+            <TopBar />
+            <div className="flex justify-between items-center w-full">
+                <div className="flex flex-col items-center bg-amber-300 w-1/5 h-dvh">
+                    <div>=============</div>
+                    <div>왼쪽 사이드바 2</div>
+                    <div>왼쪽 사이드바 3</div>
                 </div>
-                <div className="flex-none gap-6">
-                    <a className="btn btn-ghost text-lg text-white hover:bg-base-300/20">
-                        <FaGlobeAsia className="text-white size-6"/>
-                        공유
-                    </a>
-                    <button className="btn">
-                        <div className="mx-2 text-lg text">저장</div>
-                    </button>
-                    <button className="btn btn-square btn-ghost">
-                        <LuBarChartBig className="text-white size-6"/>
-                    </button>
-                    <button className="btn btn-square btn-ghost">
-                        <LuAlignJustify className="text-white size-6"/>
-                    </button>
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component"
-                                     src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
-                            </div>
-                        </div>
-                        <ul tabIndex={0}
-                            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <li><a>Logout</a></li>
-                        </ul>
+                {children}
+                <div className="flex flex-col items-center w-1/5 h-dvh bg-blue-400">
+                    <div role="tablist" className="tabs bg-neutral-300 tabs-boxed my-6 grid grid-flow-col auto-cols-fr">
+                        <a role="tab" className={cx("tab", !layoutSelected && "bg-base-100")}>Content</a>
+                        <a role="tab" className={cx("tab", layoutSelected && "bg-base-100")}>Layout</a>
                     </div>
                 </div>
             </div>
-            이게 맞나?
-            {children}
         </div>
     )
 }
