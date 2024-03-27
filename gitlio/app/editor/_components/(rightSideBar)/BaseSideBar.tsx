72b25975d5680d6ea35de1cd9d30dddc7cb69e71
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-27 18:56:36 +0900

    design: Editor Top menu layout - 3

--- gitlio/app/editor/_components/(rightSideBar)/BaseSideBar.tsx
+++ gitlio/app/editor/_components/(rightSideBar)/BaseSideBar.tsx
@@ -1,14 +1,21 @@
 import cx from "classnames";
 
 export default function BaseSideBar() {
-    const layoutSelected = true
+  const layoutSelected = true;
 
-    return (
-        <div className="flex flex-col items-center w-96 h-dvh bg-blue-400">
-            <div role="tablist" className="tabs bg-neutral-300 tabs-boxed my-6 grid grid-flow-col auto-cols-fr">
-                <a role="tab" className={cx("tab", !layoutSelected && "bg-base-100")}>Content</a>
-                <a role="tab" className={cx("tab", layoutSelected && "bg-base-100")}>Layout</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center w-96 h-dvh bg-blue-400">
+      <div
+        role="tablist"
+        className="tabs bg-neutral-300 tabs-boxed my-6 grid grid-flow-col auto-cols-fr"
+      >
+        <a role="tab" className={cx("tab", !layoutSelected && "bg-base-100")}>
+          Content
+        </a>
+        <a role="tab" className={cx("tab", layoutSelected && "bg-base-100")}>
+          Layout
+        </a>
+      </div>
+    </div>
+  );
 }
