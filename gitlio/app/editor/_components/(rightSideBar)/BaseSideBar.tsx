892cab03d07d02a16fdc748c4c4ce31f3c18c5ac
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-27 19:00:55 +0900

    design: Editor Top menu layout - change left & right sidebar

--- gitlio/app/editor/_components/(rightSideBar)/BaseSideBar.tsx
+++ gitlio/app/editor/_components/(rightSideBar)/BaseSideBar.tsx
@@ -1,7 +1,14 @@
-export default function BaseSideBar() {
-    return (
-        <div className="artboard">
+import cx from "classnames";
 
+export default function BaseSideBar() {
+    const layoutSelected = true
+
+    return (
+        <div className="flex flex-col items-center w-1/5 h-dvh bg-blue-400">
+            <div role="tablist" className="tabs bg-neutral-300 tabs-boxed my-6 grid grid-flow-col auto-cols-fr">
+                <a role="tab" className={cx("tab", !layoutSelected && "bg-base-100")}>Content</a>
+                <a role="tab" className={cx("tab", layoutSelected && "bg-base-100")}>Layout</a>
+            </div>
         </div>
     )
 }
--- gitlio/app/editor/_components/LeftSidebar.tsx
+++ gitlio/app/editor/_components/LeftSidebar.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-function LeftSidebar() {
-  return <div>LeftSidebar</div>;
+export default function LeftSidebar() {
+  return (
+    <div className="flex flex-col items-center bg-amber-300 w-56 h-dvh">
+      <div>=============</div>
+      <div>왼쪽 사이드바 2</div>
+      <div>왼쪽 사이드바 3</div>
+    </div>
+  );
 }
-
-export default LeftSidebar;
