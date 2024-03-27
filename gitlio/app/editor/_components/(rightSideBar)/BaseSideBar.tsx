5a77cde181e69f96d4c18e8dc484658a43da9748
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-27 19:19:56 +0900

    design: Editor Top menu layout - change left & right sidebar size

--- gitlio/app/editor/_components/(rightSideBar)/BaseSideBar.tsx
+++ gitlio/app/editor/_components/(rightSideBar)/BaseSideBar.tsx
@@ -4,7 +4,7 @@ export default function BaseSideBar() {
   const layoutSelected = true;
 
   return (
-    <div className="flex flex-col items-center w-1/5 h-dvh bg-blue-400">
+    <div className="flex flex-col items-center w-96 h-dvh bg-blue-400">
       <div
         role="tablist"
         className="tabs bg-neutral-300 tabs-boxed my-6 grid grid-flow-col auto-cols-fr"
