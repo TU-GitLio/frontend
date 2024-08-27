e21d942e4e9f66f8e4bb8d30135b793f60117fd4
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-27 19:46:36 +0900

    design: PendingAlert 컴포넌트 UI 퍼블리싱

--- gitlio/app/studio/statistics/insight/page.tsx
+++ gitlio/app/studio/statistics/insight/page.tsx
@@ -1,16 +1,12 @@
-import Navbar from '@/components/studio/StdNavbar';
-import Sidebar from '@/components/studio/StdSidebar';
-import Menubar from '@/components/studio/statistic/StatMenu';
+import PendingAlert from '@/components/PendingAlert';
 
 export default function StatisticsPage() {
   return (
     <div className="flex min-h-screen w-full">
-      <Sidebar />
-
-      <div className="flex flex-col flex-1">
-        <Navbar />
-        <Menubar />
-        <main className="flex-1 p-4">메인 컨텐츠 영역</main>
+      <div className="flex flex-col flex-1 items-center justify-center">
+        <main className="flex flex-col justify-center items-center w-[300px] h-[300px]">
+          <PendingAlert />
+        </main>
       </div>
     </div>
   );
